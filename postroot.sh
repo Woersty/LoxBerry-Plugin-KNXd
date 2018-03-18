ed152cf3b8cb23c70296e1ba43556e0a4bde3981
Wörsty <git@loxberry.woerstenfeld.de>
2018-03-18 21:18:44 +0100

    Duplicated Log entries
    Fixes #9

--- postroot.sh
+++ postroot.sh
@@ -5,7 +5,10 @@ date        >> $logfile
 chown loxberry $logfile
 chgrp loxberry $logfile
 chmod 666      $logfile
-echo "<INFO> Start KNXd installation, for further infos see Plugin logfile" 2>&1
-bash REPLACELBHOMEDIR/system/daemons/plugins/KNXd                    2>&1 
+echo "<INFO> Start KNXd installation" 2>&1
+tail -f $logfile &
+TAILPID=$!
+bash REPLACELBHOMEDIR/system/daemons/plugins/KNXd                  >/dev/null 2>&1 
+kill -9 $TAILPID
 echo "<INFO> Installation completed" 2>&1
 exit 0
