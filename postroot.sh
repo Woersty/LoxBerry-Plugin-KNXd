a2a2fcac9a9740191dfeb78bac94f87c1e2b09f4
Wörsty <git@loxberry.woerstenfeld.de>
2021-04-18 14:07:20 +0200

    Update postroot.sh
    Restart rpimonitor after KNXd install to monitor service

--- postroot.sh
+++ postroot.sh
@@ -11,4 +11,5 @@ TAILPID=$!
 bash REPLACELBHOMEDIR/system/daemons/plugins/KNXd                  >/dev/null 2>&1 
 kill -9 $TAILPID
 echo "<INFO> Installation completed. Please wait a while for the background processes to be completed. See Pluing log for details." 2>&1
+service rpimonitor status |grep " active " >/dev/null && echo `echo "rpimonitor service IS running, restarting"; service rpimonitor restart` || echo "rpimonitor service NOT running, no restart"
 exit 0
